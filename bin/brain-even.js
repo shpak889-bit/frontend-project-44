9d9f4f3e7dd5301bc662fa94cf591718f4550349
unknown <pashckovdanil@gmail.com>
2026-04-22 04:16:06 +0700

    ts

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import startGame from '../src/games/even.js';
 
-export default startGame;
+startGame();
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import startGame from '../src/games/prime.js';
 
-export default startGame;
+startGame();
